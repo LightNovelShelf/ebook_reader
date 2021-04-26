41c41c54f831e90af469b9def208d57f182fe57f
Eflet <eflet1986@hotmail.com>
2021-04-26 17:24:01 +0800

    添加删除功能

--- src/views/Home/BookShelf.vue
+++ src/views/Home/BookShelf.vue
@@ -22,134 +22,284 @@
     </v-app-bar>
     <v-main>
       <transition-group tag="div" type="transition" name="flip-list" class="move">
-          <template v-if="!gid">
-            <v-col cols="4" sm="4" md="3" lg="2" v-for="book in BookList" :key="book.gid || book.book_path">
-              <book-card v-on:load-book="loadBook" v-if="!book.gid" :book="book"></book-card>
-              <book-group-card v-else :books="book"></book-group-card>
-            </v-col>
-          </template>
-          <template v-else>
-            <v-col cols="4" sm="4" md="3" lg="2" v-for="book in books.data" :key="book['book_path']">
-              <book-card v-on:load-book="loadBook" :book="book"></book-card>
-            </v-col>
-          </template>
+        <template v-if="!gid">
+          <v-col cols="4" sm="4" md="3" lg="2" v-for="book in BookList" :key="book.gid || book.book_path">
+            <div
+              class="box"
+              v-longclick="() => (fab ? null : onLongTouch(book))"
+              @click="() => (fab ? toggleSelect(book) : null)"
+            >
+              <book-card
+                v-on:load-book="loadBook"
+                v-if="!book.gid"
+                :book="book"
+                :disable="fab"
+                :key="(book.gid || book.book_path) + fab"
+              ></book-card>
+              <book-group-card
+                v-else
+                :books="book"
+                :disable="fab"
+                :key="(book.gid || book.book_path) + fab"
+              ></book-group-card>
+              <v-overlay class="overlay" absolute :opacity="0.3" :value="fab">
+                <v-btn class="btn-select" :color="checkIsSelect(book) ? 'green' : 'white'" fab x-small>
+                  <v-icon>{{ icon.mdiCheckBold }}</v-icon>
+                </v-btn>
+              </v-overlay>
+            </div>
+          </v-col>
+        </template>
+        <template v-else>
+          <v-col cols="4" sm="4" md="3" lg="2" v-for="book in books.data" :key="book['book_path']">
+            <div
+              class="box"
+              v-longclick="() => (fab ? null : onLongTouch(book))"
+              @click="() => (fab ? toggleSelect(book) : null)"
+            >
+              <book-card
+                v-on:load-book="loadBook"
+                :book="book"
+                :disable="fab"
+                :key="book['book_path'] + fab"
+              ></book-card>
+              <v-overlay class="overlay" absolute :opacity="0.3" :value="fab">
+                <v-btn class="btn-select" :color="checkIsSelect(book) ? 'green' : 'white'" fab x-small>
+                  <v-icon>{{ icon.mdiCheckBold }}</v-icon>
+                </v-btn>
+              </v-overlay>
+            </div>
+          </v-col>
+        </template>
       </transition-group>
+      <template>
+        <div class="float-box">
+          <v-tooltip left>
+            <template v-slot:activator="{ on }">
+              <v-fab-transition>
+                <v-btn dark fab color="error" v-on="on" class="mb-4" @click="confirmDelete" v-if="fab" small>
+                  <v-icon>{{ icon.mdiTrashCanOutline }}</v-icon>
+                </v-btn>
+              </v-fab-transition>
+            </template>
+            <span>删除</span>
+          </v-tooltip>
+          <v-fab-transition>
+            <v-btn dark fab color="white" @click="toggleFab" v-if="fab" small>
+              <v-badge color="primary" :content="`${selectedCount}`">
+                <v-icon color="grey darken-4">{{ icon.mdiClose }}</v-icon>
+              </v-badge>
+            </v-btn>
+          </v-fab-transition>
+        </div>
+      </template>
     </v-main>
   </v-container>
 </template>
 
 <script>
-  import { icon } from '@/plugins/vuetify'
-  import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
-  import BookCard from '@/components/Home/BookCard'
-  import BookGroupCard from '@/components/Home/BookGroupCard'
+import { icon } from '@/plugins/vuetify'
+import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
+import BookCard from '@/components/Home/BookCard'
+import BookGroupCard from '@/components/Home/BookGroupCard'
+import longclick from '@/plugins/longClick'
 
-  export default {
-    name: 'Bookshelf',
-    components: { BookGroupCard, BookCard },
-    data() {
-      return {
-        showMoveDialog: false,
-        showGroupEditDialog: false,
-        icon: icon
-      }
+export default {
+  name: 'Bookshelf',
+  components: { BookGroupCard, BookCard },
+  directives: {
+    longclick
+  },
+  data() {
+    return {
+      showMoveDialog: false,
+      showGroupEditDialog: false,
+      fab: false,
+      selectedBooks: [],
+      icon: icon
+    }
+  },
+  props: {
+    gid: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    ...mapGetters(['BookList']),
+    books() {
+      return this.BookList.find((item) => item.gid === this.gid)
     },
-    props: {
-      gid: {
-        type: String,
-        default: null
-      }
+    selectedCount() {
+      return this.selectedBooks.reduce((rst, { data }) => {
+        return (data ? data.length : 1) + rst
+      }, 0)
+    }
+  },
+  methods: {
+    ...mapActions(['saveBookList']),
+    ...mapMutations(['updateBookList']),
+    openBook() {
+      console.log('从文件管理器选择一本书并打开书籍')
+      window.device?.choiceBook()
     },
-    computed: {
-      ...mapGetters(['BookList']),
-      books() {
-        return this.BookList.find((item) => item.gid === this.gid)
-      }
+    openDir() {
+      console.log('从文件管理器导入一个文件夹')
+      window.device?.choiceDir()
     },
-    methods: {
-      ...mapActions(['saveBookList']),
-      ...mapMutations(['updateBookList']),
-      openBook() {
-        console.log('从文件管理器选择一本书并打开书籍')
-        window.device?.choiceBook()
-      },
-      openDir() {
-        console.log('从文件管理器导入一个文件夹')
-        window.device?.choiceDir()
-      },
-      loadBook(book) {
-        let vue = this
-        function moveToFirst() {
-          if (vue.gid) {
-            let index = vue.BookList.findIndex((item) => item.gid === vue.gid)
-            let temp = [vue.books, ...vue.BookList]
-            temp.splice(index + 1, 1)
+    loadBook(book) {
+      let vue = this
+      function moveToFirst() {
+        if (vue.gid) {
+          let index = vue.BookList.findIndex((item) => item.gid === vue.gid)
+          let temp = [vue.books, ...vue.BookList]
+          temp.splice(index + 1, 1)
 
-            index = temp[0].data.findIndex((item) => item['book_path'] === book['book_path'])
-            let temp2 = [book,...temp[0].data]
-            temp2.splice(index + 1, 1)
-            temp[0].data = temp2
+          index = temp[0].data.findIndex((item) => item['book_path'] === book['book_path'])
+          let temp2 = [book, ...temp[0].data]
+          temp2.splice(index + 1, 1)
+          temp[0].data = temp2
 
-            vue.updateBookList(temp)
-          } else {
-            const index = vue.BookList.findIndex((item) => item['book_path'] === book['book_path'])
-            let temp = [book, ...vue.BookList]
-            temp.splice(index + 1, 1)
-            vue.updateBookList(temp)
-          }
+          vue.updateBookList(temp)
+        } else {
+          const index = vue.BookList.findIndex((item) => item['book_path'] === book['book_path'])
+          let temp = [book, ...vue.BookList]
+          temp.splice(index + 1, 1)
+          vue.updateBookList(temp)
         }
-        window.device ? (window.moveToFirst = moveToFirst) : moveToFirst()
+      }
+      window.device ? (window.moveToFirst = moveToFirst) : moveToFirst()
+    },
+    toggleFab() {
+      this.fab = !this.fab
+      if (!this.fab) {
+        this.lastMoveTarget = null
+        this.selectedBooks = []
+      }
+    },
+    toggleSelect(book, forceSelect) {
+      let key = `${book.gid}_${book.book_path}`
+      let add
+      if (forceSelect === 1) {
+        add = true
+      } else if (forceSelect === -1) {
+        add = false
+      } else {
+        add = !this.checkIsSelect(book)
+      }
+      if (add) {
+        this.selectedBooks = [...this.selectedBooks, book]
+      } else {
+        this.selectedBooks = this.selectedBooks.filter(({ gid, book_path }) => `${gid}_${book_path}` !== key)
+      }
+    },
+    onLongTouch(book) {
+      this.fab = true
+      this.toggleSelect(book, 1)
+    },
+    checkIsSelect(book) {
+      let key = `${book.gid}_${book.book_path}`
+      return !!this.selectedBooks.find(({ gid, book_path }) => `${gid}_${book_path}` === key)
+    },
+    confirmDelete() {
+      if (this.selectedCount === 0) return
+      var r = window.confirm(`确认删除选中的 ${this.selectedCount} 本书？`)
+      if (r) {
+        Promise.resolve().then(() => {
+          let books = this.BookList.filter((g) => {
+            return !this.selectedBooks.find((item) => item.gid == g.gid)
+          }).map((g) => {
+            return {
+              ...g,
+              data: (g.data || []).filter((b) => {
+                return !this.selectedBooks.find((item) => item.book_path == b.book_path)
+              })
+            }
+          })
+          this.updateBookList(books)
+          return this.$nextTick()
+        }).then(()=>{
+          this.toggleFab()
+        })
       }
     }
   }
+}
 </script>
 
 <style scoped lang="scss">
-  @import '../../assets/styles/mixin';
+@import '../../assets/styles/mixin';
 
-  .card-box {
-    position: relative;
-    cursor: pointer;
-    .overlay {
-      bottom: 60px;
-      align-items: flex-start !important;
-      justify-content: flex-end !important;
-      z-index: 3 !important;
+.card-box {
+  position: relative;
+  cursor: pointer;
+  .overlay {
+    bottom: 60px;
+    align-items: flex-start !important;
+    justify-content: flex-end !important;
+    z-index: 3 !important;
 
-      .btn-select {
-        margin-top: -16px;
-        margin-right: -10px;
-      }
-    }
-    .category-text {
-      @include ellipsis2(2);
+    .btn-select {
+      margin-top: -16px;
+      margin-right: -10px;
     }
   }
+  .category-text {
+    @include ellipsis2(2);
+  }
+}
 
-  .move {
-    display: flex;
-    flex-wrap: wrap;
-    flex: 1 1 auto;
+.move {
+  display: flex;
+  flex-wrap: wrap;
+  flex: 1 1 auto;
 
-    .flip-list-move {
-      transition: all 0.5s;
+  .flip-list-move {
+    transition: all 0.5s;
+  }
+
+  .flip-list-enter-active,
+  .flip-list-leave-active {
+    transition: all 0.5s;
+  }
+
+  .flip-list-leave-active {
+    position: absolute;
+  }
+
+  .flip-list-enter,
+  .flip-list-leave-to {
+    opacity: 0;
+    transform: scale(0);
+    max-width: 0;
+    flex-basis: 0;
+  }
+}
+.float-box {
+  position: fixed;
+  right: 24px;
+  bottom: 24px;
+  display: flex;
+  flex-direction: column;
+  z-index: 100;
+}
+.box {
+  position: relative;
+  .overlay {
+    bottom: 60px;
+    align-items: flex-start !important;
+    justify-content: flex-end !important;
+    z-index: 3 !important;
+
+    .btn-select {
+      margin-top: -16px;
+      margin-right: -10px;
     }
 
-    .flip-list-enter-active,
-    .flip-list-leave-active {
-      transition: all 0.5s;
-    }
-
-    .flip-list-leave-active {
-      position: absolute;
-    }
-
-    .flip-list-enter,
-    .flip-list-leave-to {
-      opacity: 0;
-      transform: scale(0);
-      max-width: 0;
-      flex-basis: 0;
+    pointer-events: none;
+    > .v-overlay__content {
+      pointer-events: auto;
     }
   }
+}
 </style>
