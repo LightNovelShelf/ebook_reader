946daeb25716fa09ede68468b7469c085744cb63
wuyu8512 <wuyu8512@gmail.com>
2021-09-08 16:40:58 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import './assets/style/main.scss'
+import router from './router'
+import { naive } from './plugins/naive-ui'
+
+const app = createApp(App)
+app.use(createPinia()).use(router).use(naive).mount('#app')
